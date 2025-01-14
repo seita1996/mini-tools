3e0b66d5d0e24617290f1b2a3f2687ab1f246708
Seitaro <tahara.seitaro@gmail.com>
2025-01-15 01:53:17 +0900

    sample counter app

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,17 +1,16 @@
 module Pages.Home_ exposing (view)
 
 import Html
+import UI
 import View exposing (View)
-import Html.Attributes
 
 
 view : View msg
 view =
     { title = "Mini Tools"
-    , body = 
-        [ Html.h1 [] [ Html.text "Mini Tools" ]
-        , Html.ul []
-            [ Html.li [] [ Html.a [ Html.Attributes.href "/lottery" ] [ Html.text "Lottery" ] ]
+    , body =
+        UI.layout
+            [ Html.h1 [] [ Html.text "Mini Tools" ]
+            , Html.p [] [ Html.text "Mini useful tools." ]
             ]
-        ]
     }
