39e1fbdbc890084844c2275caf5d7c2169f65b21
Seitaro <tahara.seitaro@gmail.com>
2025-01-19 13:45:02 +0900

    remove dead code

--- src/Pages/Lottery.elm
+++ src/Pages/Lottery.elm
@@ -3,15 +3,11 @@ module Pages.Lottery exposing (Model, Msg, page)
 import Effect exposing (Effect)
 import Gen.Params.Lottery exposing (Params)
 import Html exposing (button, div, h1, input, text, textarea)
-import Html.Events as Events
 import Page
 import Request
 import Shared
 import UI
 import View exposing (View)
-import Html exposing (p)
-import Html exposing (h2)
-import Html exposing (h3)
 import Html.Attributes exposing (placeholder)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onInput)
@@ -21,7 +17,7 @@ import Random
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
-page shared req =
+page shared _ =
     Page.advanced
         { init = init
         , update = update
@@ -61,9 +57,7 @@ init =
 
 
 type Msg
-    = IncrementShared
-    | DecrementShared
-    | UpdateInputText String
+    = UpdateInputText String
     | UpdateNumWinners String
     | StartLottery
     | UpdateTime Posix
@@ -71,15 +65,6 @@ type Msg
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        IncrementShared ->
-            ( model
-            , Effect.fromShared Shared.Increment
-            )
-
-        DecrementShared ->
-            ( model
-            , Effect.fromShared Shared.Decrement
-            )
         UpdateInputText text ->
             ( { model | inputText = text }, Effect.none )
         UpdateNumWinners num ->
@@ -113,7 +98,7 @@ update msg model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
@@ -122,17 +107,11 @@ subscriptions model =
 
 
 view : Shared.Model -> Model -> View Msg
-view shared model =
+view _ model =
     { title = "Lottery"
     , body =
         UI.layout
             [ UI.h1 "Lottery"
-            , p [] [ text "An advanced page uses Effects instead of Cmds, which allow you to send Shared messages directly from a page." ]
-            , h2 [] [ text "Shared Counter" ]
-            , h3 [] [ text (String.fromInt shared.counter) ]
-            , button [ Events.onClick DecrementShared ] [ text "-" ]
-            , button [ Events.onClick IncrementShared ] [ text "+" ]
-            , p [] [ Html.text "This value doesn't reset as you navigate from one page to another (but will on page refresh)!" ]
             , div []
                 [ textarea [ placeholder "Enter items, one per line", value model.inputText, onInput UpdateInputText ] []
                 , input [ placeholder "Number of winners", value model.numWinners, onInput UpdateNumWinners ] []
